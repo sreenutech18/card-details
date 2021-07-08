5f0e4adbc0aefbaf37f6697614894f63b77b05a6
sreenivas <sreenu.sreenutech@gmail.com>
2021-07-08 08:27:39 +0530

    added to lombok functions

--- card-details-service-sb/src/main/java/com/citi/membership/card/exception/CardDetailsReqValidationExcep.java
+++ card-details-service-sb/src/main/java/com/citi/membership/card/exception/CardDetailsReqValidationExcep.java
@@ -1,8 +1,11 @@
 package com.citi.membership.card.exception;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 public class CardDetailsReqValidationExcep extends Exception{
 	
 	private String errorMsg; 
